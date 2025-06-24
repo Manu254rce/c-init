f95990dc4a73dc65141676860465d4c72dc3e6ba
Manu254rce <emmanuellamira20@outlook.com>
2025-06-25 01:50:20 +0300

    chore: added boilerplate header file

---
+++ include/main.h
@@ -0,0 +1,22 @@
+# ifndef MAIN_H
+# define MAIN_H
+
+/* Header files */
+
+# include <stdio.h>
+# include <stdlib.h>
+/* # include <unistd.h> */
+
+/* Constants/ macros */
+
+
+
+/* Data structs/ typedefs (Betty-compliant) */
+
+
+
+/* Function prototypes */
+
+
+
+# endif /* MAIN_H */
